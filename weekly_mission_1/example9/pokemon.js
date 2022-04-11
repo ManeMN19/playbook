8251120f960bfa87c9a9581c14c3183bab89d8d2
ManeMN19 <manuel@maciasnavarro.mx>
2022-04-10 21:16:36 -0500

    Example 9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,16 @@
+class Pokemon {
+  constructor(name) {
+    // this es una variable para referenciar el valor del contexto local de esta clase
+    this.name = name // Estás variables se le conocen como atributos
+  }
+
+  sayHello () {
+    console.log(`Mi pokemon ${this.name} te saluda!!!`)
+  }
+
+  sayMessage (message) {
+    console.log(`Mi pokemon ${this.name} dice: ${message}`)
+  }
+}
+
+module.exports = Pokemon

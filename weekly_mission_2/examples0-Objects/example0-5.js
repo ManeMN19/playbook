53f25aff8f2206478212c4b87571cb9a53a6363b
ManeMN19 <manuel@maciasnavarro.mx>
2022-04-22 21:41:46 -0500

    Example 0-5

--- weekly_mission_2/examples0-Objects/example0-5.js
+++ weekly_mission_2/examples0-Objects/example0-5.js
@@ -1,20 +1,9 @@
-// const myPokemon = {
-//     name: "Pikachu",
-//     sayHelloToMyPokemon: function(yourPokemon){
-//       console.log(`${this.name} dice "Hola" a ${yourPokemon}`)
-//     }
-//   }
-  
-//   console.log("Ejemplo 5: Objeto con método que recibe parámetros")
-//   myPokemon.sayHelloToMyPokemon("Charizard");
-
-
-  const myPet = {
-    name: "Woopa",
-    sayHelloToMyPet: function(yourPet){
-      console.log(`${this.name} say's hello to ${yourPet}`)
+const myPokemon = {
+    name: "Pikachu",
+    sayHelloToMyPokemon: function(yourPokemon){
+      console.log(`${this.name} dice "Hola" a ${yourPokemon}`)
     }
-  }
+  }  
   
   console.log("Ejemplo 5: Objeto con método que recibe parámetros")
-  myPet.sayHelloToMyPet("Tulio")
+  myPokemon.sayHelloToMyPokemon("Charizard");

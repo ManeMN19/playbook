59ed38d884be270e2fda8395716e51247376542f
ManeMN19 <manuel@maciasnavarro.mx>
2022-04-22 21:33:18 -0500

    Se agrega Example 0-3

---
+++ weekly_mission_2/examples0/examples0-3.js
@@ -0,0 +1,21 @@
+const myObject3 = {
+    name: "Manuel Navarro",
+    age: 30,
+    nicknames: [
+        "Mane",
+        "Manu",
+        "Manecito"
+    ],
+    address: {
+      zip_code: "45080",
+      street: "Prol. Boyero",
+      city: "Zapopan",
+      state: "Jalisco"
+    }
+}
+console.log("Ejemplo 3: Crear un objeto con propiedades diferentes");
+console.log("Mi Objeto: ");
+console.log(myObject3);
+console.log(`\nNombre: ${myObject3.name}\n`);
+console.log("Dirección: ");
+console.log(myObject3.address);
